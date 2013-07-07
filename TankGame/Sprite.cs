b8fd651698ef91d4c940573e7f25adf6cf7b15a5
Scott <sco50000@gmail.com>
2013-07-06 20:04:02 -0600

    Add a few more Sprite fields.

--- TankGame/Sprite.cs
+++ TankGame/Sprite.cs
@@ -7,31 +7,29 @@ namespace TankGame
     class Sprite
     {
         public Texture2D Texture { get; set; }
-        public Vector2 Position { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
         public float Speed { get; set; }
         public float Heading { get; set; }
 
-        public Vector2 Velocity
-        {
-            get
-            {
-                float velX = Speed * (float) Math.Cos(Heading);
-                float velY = Speed * (float) Math.Sin(Heading);
-                return new Vector2(velX, velY);
-            }
-        }
+        public Vector2 Position { get { return new Vector2(X, Y); } }
+        public float VelX { get { return Speed * (float)Math.Cos(Heading); } }
+        public float VelY { get { return Speed * (float)Math.Sin(Heading); } }
+        public Vector2 Velocity { get { return new Vector2(VelX, VelY); } }
 
         public Sprite(Texture2D texture)
         {
             Texture = texture;
-            Position = new Vector2(0, 0);
+            X = 0;
+            Y = 0;
             Speed = 0;
             Heading = 0;
         }
 
         public void Advance()
         {
-            Position += Velocity;
+            X += VelX;
+            Y += VelY;
         }
     }
 }
